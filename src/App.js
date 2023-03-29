d8db9b6d90c174665d2363a491cbfd40dc52f0c2
Ayub <egalayub2@gmail.com>
2023-03-29 15:37:57 -0400

    created basic ResourcePage and implemented tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+  <Helmet>
+      <title> Resource Page</title>
+      </Helmet>
+      <main className="bg-gray-700">
+      <section className=' min-h-screen'>
+      <div className="font-RaleWay text-yellow-500 text-3xl text-center py-10">
+        <h1 >
+       Here are Some Resources 
+        </h1>
+        <ul className="font-RaleWay text-yellow-500 text-xl text-center pt-10">
+          <li>
+            <a href="https://www.tasteofhome.com/article/cooking-measurement-conversion/">Measurment Converter</a>
+         </li>
+         <li>
+            <a href="https://www.timeanddate.com/timer/">Online Timer</a>
+         </li>
+
+        </ul>
+      </div>
+      </section>
+      </main>
     </div>
+    
   );
 }
 
